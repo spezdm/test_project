4aca71c8e5fdea1fd8a39aad7f938fa7267b81c8
spezdm <385*583NexDim>
2020-03-15 23:40:13 +0200

    Test

--- src/test/java/ua/com/prestaShopAutomationQatestlab/TestFirst.java
+++ src/test/java/ua/com/prestaShopAutomationQatestlab/TestFirst.java
@@ -1,12 +1,37 @@
 package ua.com.prestaShopAutomationQatestlab;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TestFirst {
+public class TestFirst extends WebDriverSettings {
     @Test
-    public void firstTest() {
-        System.getProperty("webdriver.chrome.driver","");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("http://prestashop-automation.qatestlab.com.ua/ru/");
+    public void OpenSite() {
+    System.out.println("Открыть главную страницу сайта:\n");
+    //driver.manage().window().setSize(new Dimension(100,100));
+    driver.get("http://prestashop-automation.qatestlab.com.ua/ru/");
+
+    }
+    @Test
+    public void Price () {
+    System.out.println("Выполнить проверку, что цена продуктов в секции " +
+                        "\"Популярные товары\" указана в соответствии с" +
+                       " установленной валютой в шапке сайта (USD, EUR, UAH): ");
+    driver.get("http://prestashop-automation.qatestlab.com.ua/ru/");
+   // WebElement header = driver.findElement(By.id("header"));
+   // header.findElement(By.xpath("//*[@id='_desktop_currency_selector']//i"));
+    WebElement current = driver.findElement(By.xpath("//span[contains(text(), 'UAH ₴')]"));
+    String currentStr = current.getText().trim();
+    if (currentStr.equals("UAH ₴")) {
+        System.out.println("Популярные товары указаны в валюте: " + currentStr);
+        }
     }
 }
+
+
